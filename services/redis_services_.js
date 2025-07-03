6ad91f621215468229f91df1ad745d231fb160df
Abhishek Ganeshe <abhishekganeshe33@gmail.com>
2025-07-04 01:57:30 +0530

    small changes

--- services/redis_services_.js
+++ services/redis_services_.js
@@ -1,6 +1,6 @@
 const { createClient }= require('@redis/client')
-const redisURL=  process.env.Redis_URL_Live 
-// const redisURL=  process.env.Redis_URL
+// const redisURL=  process.env.Redis_URL_Live 
+const redisURL=  process.env.Redis_URL
 
 
 exports.redisClient= createClient({
