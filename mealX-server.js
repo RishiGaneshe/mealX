d76f8c1fb121bf25d908c15f32599566bd6b08b1
Abhishek Ganeshe <abhishekganeshe33@gmail.com>
2025-07-18 03:51:07 +0530

    added a health check route

--- mealX-server.js
+++ mealX-server.js
@@ -48,7 +48,9 @@ app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 
 
-
+app.get('/', (req, res) => {
+    res.status(200).json({ success: true})
+}) 
 app.use('/', USER)
 app.use('/user', tokenAuthentication, AUTHENTICATE)
 app.use('/owner', setRoleOwner, tokenAuthentication, authorizationRoleMiddleware, OWNER )
